fdd2acb15e345f7049ee4580ad55cc729fc1ad51
kimjin0 <kimjin0@naver.com>
2020-06-03 15:26:51 +0900

    7. LiveCycleSample

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 // import React, { Fragment } from "react";
 import React, { Component } from "react";
-import Iteration from "./Iteration";
-
+import LifeCycle from "./LifeCycle";
+function getRandomColor() {
+    return "#" + Math.floor(Math.random() + 16777215).toString(16);
+}
 class App extends Component {
+    state = {
+        color: "#000000",
+    };
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor(),
+        });
+    };
+
     render() {
         return (
             <div>
-                <Iteration />
+                <button onClick={this.handleClick}>랜덤색상</button>
+                <LifeCycle color={this.state.color} />
             </div>
         );
     }
