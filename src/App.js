7f07aa595d2898663d8d119ad55ab030e726d0fc
kimjin0 <kimjin0@naver.com>
2020-06-03 18:44:01 +0900

    8.2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 // import React, { Fragment } from "react";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Info from "./Info";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <Info />
-            </div>
-        );
-    }
-}
+const App = () => {
+    const [visible, setVisible] = useState(false);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setVisible(!visible);
+                }}
+            >
+                {visible ? "숨기기" : "보이기"}
+            </button>
+            {visible && <Info />}
+        </div>
+    );
+};
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Info />
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Info = () => {
     const [name, setName] = useState("");
-    const [nickname, setNickname] = useState(".
-    ");
+    const [nickname, setNickname] = useState(".");
+
+    useEffect(() => {
+        // console.log("렌더링이 완료되었습니다.");
+        // console.log({
+        //     name,
+        //     nickname,
+        // console.log("1 >" + name);
+        console.log("effect");
+        console.log(name);
+        return () => {
+            console.log("cleanup");
+            console.log(name);
+        };
+    });
 
     const onChangeName = (e) => {
         setName(e.target.value);
