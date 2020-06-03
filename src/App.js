94e50998df279028924e619dcffbd392307f78fd
kimjin0 <kimjin0@naver.com>
2020-06-03 17:41:18 +0900

    8.1 hooks - useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 // import React, { Fragment } from "react";
 import React, { Component } from "react";
-import LifeCycle from "./LifeCycle";
-function getRandomColor() {
-    return "#" + Math.floor(Math.random() + 16777215).toString(16);
-}
-class App extends Component {
-    state = {
-        color: "#000000",
-    };
-    handleClick = () => {
-        this.setState({
-            color: getRandomColor(),
-        });
-    };
+import Info from "./Info";
 
+class App extends Component {
     render() {
         return (
             <div>
-                <button onClick={this.handleClick}>랜덤색상</button>
-                <LifeCycle color={this.state.color} />
+                <Info />
             </div>
         );
     }
