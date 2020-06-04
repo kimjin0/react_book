e8bd0f074067b4c32126c7276eb8f66d34cfe049
kimjin0 <kimjin0@naver.com>
2020-06-04 11:24:00 +0900

    8.장  useReducer 까지 하고 중단함..

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 // import React, { Fragment } from "react";
 import React, { useState } from "react";
-import Info from "./Info";
+import Counter from "./UseReducer";
 
 const App = () => {
-    const [visible, setVisible] = useState(false);
-    return (
-        <div>
-            <button
-                onClick={() => {
-                    setVisible(!visible);
-                }}
-            >
-                {visible ? "숨기기" : "보이기"}
-            </button>
-            {visible && <Info />}
-        </div>
-    );
+    return <Counter />;
 };
+// const App = () => {
+//     const [visible, setVisible] = useState(false);
+//     return (
+//         <div>
+//             <button
+//                 onClick={() => {
+//                     setVisible(!visible);
+//                 }}
+//             >
+//                 {visible ? "숨기기" : "보이기"}
+//             </button>
+//             {visible && <Info />}
+//         </div>
+//     );
+// };
+
 // class App extends Component {
 //     render() {
 //         return (
 //             <div>
-//                 <Info />
+//                 <Counter />
 //             </div>
 //         );
 //     }
